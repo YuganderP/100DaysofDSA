0bc88601236f18ea47e30647a60c91d63f8bd287
yugander <yuganderpilli@outlook.com>
2024-03-28 01:27:42 +0530

    sample

--- basics.cpp
+++ basics.cpp
@@ -2,4 +2,42 @@
 using namespace std;
 int main()
 {
+    // Datatypes in cpp
+    // int
+    // long
+    // long long
+    // float datatype
+    // string , string s
+    //  getline to get space
+
+    // getline(cin, string);
+
+    // character char c
+    // conditionals
+
+    // if else else if
+
+    //  switch statement
+
+    // switch(){
+    //     case a:
+    //     break;
+    //     case b:
+    //     break;
+
+    //     default:
+
+    // }
+    // Arrays
+    // consists of simillar datatypes in contagious memory location
+    // int arr[10];
+    // Strings
+    //  For loop
+    // while loop
+    // DO while loop
+    // functions
+
+    // int sample(int a, int b){
+    //     return a+b
+    // }
 }
